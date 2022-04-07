352fdbc1f09ff4d0de8bd6157bc0fec61c50e357
Coder5424 <crysis1228@mail.ru>
2022-04-07 19:15:27 +0300

    Local settings

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -127,3 +127,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("No local file")
