f09a30d74d41c99fa302cbed7325d6339dc4123e
Coder5424 <crysis1228@mail.ru>
2023-04-05 01:15:05 +0300

    Add sort for project by number

--- portfolio/views.py
+++ portfolio/views.py
@@ -3,7 +3,7 @@ from .models import Project
 
 
 def HomePage(request):
-    projects = Project.objects.all()
+    projects = Project.objects.order_by('project_number')
     return render(request, 'portfolio/HomePage.html', {'projects': projects})
 
 
