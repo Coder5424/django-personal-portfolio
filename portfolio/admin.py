4cd3135da5720f4876a6b9bcdcaed2f9ea1bddf7
Coder5424 <crysis1228@mail.ru>
2023-04-05 01:26:35 +0300

    Add sort projects by id

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -2,8 +2,4 @@ from django.contrib import admin
 from .models import Project
 
 
-# class MyModelAdmin(admin.ModelAdmin):
-#     list_display = ('id', 'title', 'description', 'image', 'url', )
-
-
 admin.site.register(Project)
